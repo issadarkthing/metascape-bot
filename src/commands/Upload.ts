536d6bf37a67affd5420213c89503289bb9eb552
raziman <raziman.terra@gmail.com>
2022-01-17 21:33:42 +0800

    add Upload command

---
+++ src/commands/Upload.ts
@@ -0,0 +1,27 @@
+import { Command } from "@jiman24/commandment";
+import { Message } from "discord.js";
+import { Prompt } from "@jiman24/discordjs-prompt";
+import { client } from "..";
+
+export default class extends Command {
+  name = "upload";
+
+  async exec(msg: Message) {
+
+    const prompt = new Prompt(msg);
+    const image = (await prompt.collect("Please upload nft")).attachments.first();
+
+    if (!image) {
+      throw new Error("no nft was provided");
+    }
+
+    const id = await prompt.ask("Please give an id to this nft");
+
+    if (client.nft.has(id)) {
+      throw new Error(`id "${id}" already exists`);
+    }
+
+    client.nft.set(id, image.url);
+    msg.channel.send(`Successfully saved nft`);
+  }
+}
