3c5bd2f00c36b871b78e8f078c65932d85d6da1b
raziman <raziman.terra@gmail.com>
2022-01-22 10:50:30 +0800

    change leaderboard to rank by xp

--- src/commands/Leaderboard.ts
+++ src/commands/Leaderboard.ts
@@ -1,25 +1,25 @@
 import { Command } from "@jiman24/commandment";
 import { Message, MessageEmbed } from "discord.js";
 import { client } from "../index";
-import { bold, currency } from "../utils";
+import { bold } from "../utils";
 
 export default class extends Command {
   name = "leaderboard";
   aliases = ["l"];
-  description = "show leaderboard of rich players";
+  description = "show leaderboard of high xp players";
 
   exec(msg: Message) {
 
     const player = client.players.array()
-      .sort((a, b) => b.coins - a.coins)
-      .map((x, i) => `${i + 1}. ${x.name} \`${x.coins}\``)
+      .sort((a, b) => b.xp - a.xp)
+      .map((x, i) => `${i + 1}. ${x.name} \`${x.xp}\``)
       .slice(0, 10)
       .join("\n");
 
     const embed = new MessageEmbed()
       .setColor("RANDOM")
       .setTitle("Leaderboard")
-      .setDescription(bold(`Name | ${currency}\n`) + player);
+      .setDescription(bold(`Name | XP\n`) + player);
 
     msg.channel.send({ embeds: [embed] });
   }
