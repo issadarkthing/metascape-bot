e658657ef45e0f5e3b9f9b90fff21d7e39fa9ed9
raziman <raziman.terra@gmail.com>
2022-01-22 18:54:51 +0800

    add import

--- src/structure/Client.ts
+++ src/structure/Client.ts
@@ -1,6 +1,7 @@
 import { CommandManager } from "@jiman24/commandment";
 import { Client as DiscordClient } from "discord.js";
 import Enmap from "enmap";
+import { Nft } from "./Nft";
 
 export class Client extends DiscordClient {
   players = new Enmap("Player");
