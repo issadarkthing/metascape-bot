11eda941ea546212d43fe34a69e1a55612cd9c1b
raziman <raziman.terra@gmail.com>
2022-02-02 18:26:59 +0800

    update leaderboard command

--- src/commands/DuelLeaderboard.ts
+++ src/commands/DuelLeaderboard.ts
@@ -1,27 +1,27 @@
 import { Command } from "@jiman24/commandment";
 import { Message, MessageEmbed } from "discord.js";
-import { client } from "../index";
+import { client } from "..";
 import { bold } from "../utils";
 
-export default class extends Command {
-  name = "leaderboard";
-  aliases = ["l"];
-  description = "show leaderboard of high xp players";
 
-  exec(msg: Message) {
+
+export default class extends Command {
+  name = "duelleaderboard";
+  description = "show leaderboard with most duel wins";
+
+  async exec(msg: Message) {
 
     const player = client.players.array()
-      .sort((a, b) => b.xp - a.xp)
-      .map((x, i) => `${i + 1}. ${x.name} \`${x.xp}\``)
+      .sort((a, b) => b.duelWins - a.duelWins)
+      .map((x, i) => `${i + 1}. ${x.name} \`${x.duelWins}\``)
       .slice(0, 10)
       .join("\n");
 
     const embed = new MessageEmbed()
       .setColor("RANDOM")
-      .setTitle("Coin Leaderboard")
-      .setDescription(bold(`Name | XP\n`) + player);
+      .setTitle("Duel Leaderboard")
+      .setDescription(bold(`Name | Wins\n`) + player);
 
     msg.channel.send({ embeds: [embed] });
   }
 }
-
